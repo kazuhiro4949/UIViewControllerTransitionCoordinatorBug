d8668edfca3330b378d0931f38e9d91ee0b6451b
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2018-09-17 17:02:14 +0900

    first commit

---
+++ UIViewControllerTransitionCoordinatorBug/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  UIViewControllerTransitionCoordinatorBug
+//
+//  Created by Kazuhiro Hayashi on 2018/09/17.
+//  Copyright © 2018 kazuhiro hayashi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var contentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
